fd117360c978bc14dd50e2fcce6dc39eb7c6f6f2
Nb007git <140313124+Nb007git@users.noreply.github.com>
2023-08-06 23:27:57 +0530

    Add files via upload

---
+++ Assignment 1_Basic maths.m
@@ -0,0 +1,20 @@
+%calculate
+a = 100;
+b = 100;
+c = 100;
+d = 100;
+
+e = input ("Enter a number ");
+f = input ("Enter a number ");
+g = input ("Enter a number ");
+h = input ("Enter a number ");
+
+p = a+e;
+q = b-f;
+r = c*g;
+s = d/h;
+
+fprintf ("The addition is = %f \n", p );
+fprintf ("The subtraction is = %f \n", q );
+fprintf ("The multiplication is = %f \n", r );
+fprintf ("The division is = %f \n", s );
